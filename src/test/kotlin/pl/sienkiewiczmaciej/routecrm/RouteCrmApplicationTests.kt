f767694fd9c185893387fb0f419529b07aa436ef
MaciejSienkiewicz <maciej.sienkiewicz@allegro.pl>
2025-10-11 19:31:23 +0200

    first commit

---
+++ src/test/kotlin/pl/sienkiewiczmaciej/routecrm/RouteCrmApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.sienkiewiczmaciej.routecrm
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RouteCrmApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
